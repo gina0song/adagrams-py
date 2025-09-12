6b87c3997ac079796eab1450c4ee3a6e27133566
Gina Song <gina0song@gmail.com>
2025-09-12 13:14:15 -0700

    passed wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -70,7 +70,26 @@ def uses_available_letters(word, letter_bank):
     
 
 def score_word(word):
-    pass
+    LETTER_SCORES = { "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1, 
+                     "D": 2, "G": 2, "B": 3, 
+                     "C": 3, "M": 3, "P": 3, 
+                     "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4, 
+                     "K": 5, 
+                     "J": 8, "X": 8, 
+                     "Q": 10, "Z": 10 
+    }
+    score = 0
+    if not word:
+        return score
+    else:
+        
+        for letter in word.upper():
+            score += LETTER_SCORES[letter]
+
+    if len(word) >= 7:
+        score += 8
+    return  score
+    #pass
 
 def get_highest_word_score(word_list):
     pass
